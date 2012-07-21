4149a646e2fc20719b9cbff0d3ed48aeb16e3e30
Spyros Papageorgiou <spyros.papageorgiou@gmail.com>
2012-07-21 16:59:55 +0300

    Stuff

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,17 @@ import views.html.*;
 public class Application extends Controller {
   
   public static Result index() {
-    return ok(index.render());
+    return ok(main.render(new String("Hello World"),new String("				<div class=\"post\">" + 
+					"<h1 class=\"title\"> Title </h1>" +
+					"<div class=\"divider\"></div>" +
+					"<p class=\"date\">2 April 2010</p>" +
+					"<div class=\"divider\"></div>" +
+					"<div class=\"post_entry\"" +
+					"	<p> Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a Testa asd a </p>" +
+					"</div>" +
+					"<div class=\"divider\"></div>" +
+					"<br />" +
+				"</div>")));
   }
   
 }
---
+++ app/models/Post.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package models;
+
+/**
+ * @author spyros
+ *
+ */
+public class Post {
+
+}
